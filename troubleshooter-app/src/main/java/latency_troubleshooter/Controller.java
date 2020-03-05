d1ce4f04bc233515abe8b131a9d227f26c347ef6
Prashanth Belathur <pbelathur@pivotal.io>
2020-03-05 11:40:59 -0600

    refactor

--- troubleshooter-app/src/main/java/latency_troubleshooter/Controller.java
+++ troubleshooter-app/src/main/java/latency_troubleshooter/Controller.java
@@ -2,45 +2,24 @@ package latency_troubleshooter;
 
 import io.micrometer.core.annotation.Timed;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
-import java.util.Map;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
-    private static Logger log = LoggerFactory.getLogger(Controller.class);
-
-    private AppConfig appConfig;
     private ResponseGenerator responseGenerator;
 
-    @GetMapping("/generate")
-    @Timed(description = "Time spent generating random response")
-    private Map generate() {
-
-        Map map = new HashMap();
-        map.put("total random responses generated",
-                responseGenerator.generate(appConfig.getResultsGenerationSize()).size());
-        return map;
-    }
-
-    @GetMapping("/generateJson")
-    @Timed(description = "Time spent generating JSON response")
-    private List<Response> generateBigJSON() {
-        
-        return responseGenerator.generate(appConfig.getResultsGenerationSize());
-    }
-
-    @PutMapping("/responseSize/{size}")
-    private AppConfig updateResponseSize(@PathVariable int size) {
-
-        appConfig.setResultsGenerationSize(size);
-        return appConfig;
+    @GetMapping("/generate/{size}")
+    @Timed(description = "Time spent generating *random* JSON response")
+    private List<Response> generateJSON(@PathVariable int size) {
+        return responseGenerator.generate(size);
     }
 }
