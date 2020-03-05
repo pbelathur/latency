a1e7ab3813c3cb672ddd851f4a148ac5b9881a19
Prashanth Belathur <pbelathur@pivotal.io>
2020-03-05 10:37:38 -0600

    add REST endpoint to generate small/big JSONs

--- troubleshooter-app/src/main/java/latency_troubleshooter/Controller.java
+++ troubleshooter-app/src/main/java/latency_troubleshooter/Controller.java
@@ -4,15 +4,15 @@ import io.micrometer.core.annotation.Timed;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class Controller {
 
     private static Logger log = LoggerFactory.getLogger(Controller.class);
@@ -20,10 +20,21 @@ public class Controller {
     private AppConfig appConfig;
     private ResponseGenerator responseGenerator;
 
-    @GetMapping("/invoke")
-    @Timed(description = "Time spent generating JSON response in generateJSON()")
-    private List<Response> generateJSON() {
-       return responseGenerator.generate(appConfig.getResultsGenerationSize());
+    @GetMapping("/generate")
+    @Timed(description = "Time spent generating random response")
+    private Map generate() {
+
+        Map map = new HashMap();
+        map.put("total random responses generated",
+                responseGenerator.generate(appConfig.getResultsGenerationSize()).size());
+        return map;
+    }
+
+    @GetMapping("/generateJson")
+    @Timed(description = "Time spent generating JSON response")
+    private List<Response> generateBigJSON() {
+        
+        return responseGenerator.generate(appConfig.getResultsGenerationSize());
     }
 
     @PutMapping("/responseSize/{size}")
